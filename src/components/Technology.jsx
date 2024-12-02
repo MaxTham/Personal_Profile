1d44381f8b90984c52a3e53af35d51ad265c3e54
MaxTham <wingfei5778@gmail.com>
2024-12-02 21:13:58 +0800

    Initial commit

---
+++ src/components/Technology.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { RiJavascriptFill } from "react-icons/ri";
+import { FaPhp } from "react-icons/fa";
+import { TbBrandCSharp } from "react-icons/tb";
+import { FaPython } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { RiReactjsLine } from "react-icons/ri";
+import { FaVuejs } from "react-icons/fa";
+import { TbSql } from "react-icons/tb";
+import { DiPostgresql } from "react-icons/di";
+import { animate, motion } from "framer-motion";
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
+const Technology = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h1>
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaHtml5 className="text-7xl text-orange-600" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaCss3Alt className="text-7xl text-blue-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiJavascriptFill className="text-7xl text-yellow-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaPhp className="text-7xl text-indigo-800" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TbBrandCSharp className="text-7xl text-violet-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaPython className="text-7xl text-slate-50" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaNodeJs className="text-7xl text-emerald-600" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaVuejs className="text-7xl text-emerald-600" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(7)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TbSql className="text-7xl text-sky-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(7.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <DiPostgresql className="text-7xl text-sky-600" />
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Technology;
